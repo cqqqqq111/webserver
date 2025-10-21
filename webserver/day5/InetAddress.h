57e690a1605aa29f8535b153ce39ba38397d9d4e
cqqqqq111 <3265244172@qq.com>
2025-10-21 21:09:33 +0800

    day 5 更新

---
+++ webserver/day5/InetAddress.h
@@ -0,0 +1,11 @@
+#pragma onc
+#include<arpa/inet.h>
+ 
+class InetAddress{
+    public:
+        struct sockaddr_in addr;
+        socklen_t addr_len;
+        InetAddress();
+        InetAddress(const char*,uint16_t);
+        ~InetAddress();
+};

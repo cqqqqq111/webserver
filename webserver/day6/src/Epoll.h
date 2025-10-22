408a76da51723d35c8b6b34a34963a4f838b3bd8
cqqqqq111 <3265244172@qq.com>
2025-10-22 18:39:41 +0800

    https://github.com/cqqqqq111/webserver.git

---
+++ webserver/day6/src/Epoll.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<sys/epoll.h>
+#include<vector>
+
+class Channel;
+class Epoll{
+    private:
+        int epfd;
+        struct epoll_event *events;
+    public:
+        Epoll();
+        ~Epoll();
+        void addFd(int fd,uint32_t op);
+        void updateChannel(Channel*);
+
+        std::vector<Channel*>poll(int timeout=-1);
+};
---
+++ webserver/day6/src/InetAddress.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<arpa/inet.h>
+ 
+class InetAddress{
+    public:
+        struct sockaddr_in addr;
+        socklen_t addr_len;
+        InetAddress();
+        InetAddress(const char*,uint16_t);
+        ~InetAddress();
+};
